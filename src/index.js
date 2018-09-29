b70a786cd5920875b535d672b478abca98d17e9c
Sches <naste4ka11.11@gmail.com>
2018-09-29 23:07:47 +0300

    Solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function getZerosCount(number) {
   // your implementation
+   let count = 0;
+   while(number >= 5)
+   {
+     count += ~~(number / 5);
+     number /= 5;
+   }
+   return count;
 }
